dde3c56da4c2a160db881a5de37b515233ffc08c
Daniel Watkins <daniel.watkins@canonical.com>
2016-12-13 18:08:16 +0000

    Return filenames as names of PodcastItems

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,42 @@ fn main() {
     println!("Hello, world!");
 }
 
-struct PodcastItem {}
+struct PodcastItem {
+    name: String,
+}
 
 fn get_target_items(target_directory: &std::path::Path) -> io::Result<Vec<PodcastItem>> {
     let mut items = Vec::new();
     let entries = fs::read_dir(target_directory).unwrap();
     for entry in entries {
         let entry = entry.unwrap();
-        if fs::metadata(entry.path()).unwrap().is_dir() {
+        let path = entry.path();
+        if fs::metadata(&path).unwrap().is_dir() {
             continue;
         }
-        match entry.path().extension() {
+        match path.extension() {
             Some(x) if x == OsStr::new("mp3") => (),
             _ => continue,
         }
-        items.push(PodcastItem {});
+        items.push(PodcastItem {
+            name: String::from(
+                      path.file_name().unwrap().to_str().unwrap())});
     }
     Ok(items)
 }
 
 #[cfg(test)]
 mod test {
+    use std::collections::HashSet;
     use std::fs::File;
 
     use tempdir::TempDir;
 
     use get_target_items;
 
-    fn setup_tmpdir_with_items(items: Vec<&str>) -> TempDir {
+    fn setup_tmpdir_with_items(items: &Vec<&str>) -> TempDir {
         let tmpdir = TempDir::new("test").expect("create temp dir");
-        for filename in &items {
+        for filename in items {
             let full_path = tmpdir.path().join(filename);
             File::create(full_path).expect("create file");
         }
@@ -46,7 +52,7 @@ mod test {
 
     #[test]
     fn list_empty_directory() {
-        let tmpdir = setup_tmpdir_with_items(Vec::new());
+        let tmpdir = setup_tmpdir_with_items(&Vec::new());
 
         let items = get_target_items(tmpdir.path()).unwrap();
         assert_eq!(0, items.len())
@@ -54,7 +60,7 @@ mod test {
 
     #[test]
     fn list_single_item() {
-        let tmpdir = setup_tmpdir_with_items(vec!["first.mp3"]);
+        let tmpdir = setup_tmpdir_with_items(&vec!["first.mp3"]);
 
         let items = get_target_items(tmpdir.path()).unwrap();
         assert_eq!(1, items.len())
@@ -62,7 +68,7 @@ mod test {
 
     #[test]
     fn non_mp3_files_ignored() {
-        let tmpdir = setup_tmpdir_with_items(vec!["first.mp3", "second.not"]);
+        let tmpdir = setup_tmpdir_with_items(&vec!["first.mp3", "second.not"]);
 
         let items = get_target_items(tmpdir.path()).unwrap();
         assert_eq!(1, items.len())
@@ -70,9 +76,27 @@ mod test {
 
     #[test]
     fn non_extension_files_ignored() {
-        let tmpdir = setup_tmpdir_with_items(vec!["first.mp3", "second"]);
+        let tmpdir = setup_tmpdir_with_items(&vec!["first.mp3", "second"]);
 
         let items = get_target_items(tmpdir.path()).unwrap();
         assert_eq!(1, items.len())
+    }
+
+    #[test]
+    fn filenames_returned_as_names() {
+        let expected_names = vec!["first.mp3", "second.mp3"];
+        let tmpdir = setup_tmpdir_with_items(&expected_names);
+
+        let items = get_target_items(tmpdir.path()).unwrap();
+        assert_eq!(2, items.len());
+
+        let mut actual_names_set = HashSet::new();
+        actual_names_set.extend(items.iter().map(|item| item.name.clone()));
+        let mut expected_names_set = HashSet::new();
+        for name in expected_names.iter() {
+            expected_names_set.insert(String::from(name.clone()));
+        }
+
+        assert_eq!(expected_names_set, actual_names_set)
     }
 }
